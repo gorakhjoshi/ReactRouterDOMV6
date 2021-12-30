2cdd770133c1bfa1bc7fc80ac9d7e536761aed82
Gorakh Raj Joshi <gorakh626@gmail.com>
2021-12-30 19:25:49 +0545

    Router, Routes and Route API of ReactRouterDOM

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 function App() {
-  return <div>Hello</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path='/home' element={<div>Hello React Router DOM</div>} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
